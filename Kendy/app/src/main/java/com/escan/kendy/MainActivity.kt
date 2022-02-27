c1549969f2f383842e628f9645e655b25b7d1768
Escan-DNMZ <84273839+Escan-DNMZ@users.noreply.github.com>
2022-02-27 19:13:07 +0300

    .

---
+++ Kendy/app/src/main/java/com/escan/kendy/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.escan.kendy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity() {
+
+    var score = 0
+    val imageArray = ArrayList<ImageView>()
+    lateinit var handler:Handler
+    lateinit var runnable: Runnable
+    lateinit var countDownTimer: CountDownTimer
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        countDownTimer = object : CountDownTimer(15000,1000) {
+            override fun onFinish() {
+                textView.text = "0"
+                textView2.text = "Score: 0"
+
+                val alert = AlertDialog.Builder(this@MainActivity)
+                alert.setTitle("Game Over")
+                alert.setMessage("Are you want restart ?")
+                alert.setPositiveButton("Yes"){ dialog,which ->
+                    // Restart
+                    val intent = intent
+                    finish()
+                    startActivity(intent)
+
+                }
+                alert.setNegativeButton("No"){ dialog,which ->
+                    Toast.makeText(this@MainActivity, "GameOver", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+                alert.show()
+            }
+
+            override fun onTick(p0: Long) {
+                textView.text = "Score: ${p0/1000}"
+            }
+        }.start()
+    }
+
+    fun increaseScore(view: View){
+        score = score + 1
+        textView2.text = "$score"
+
+    }
+}
