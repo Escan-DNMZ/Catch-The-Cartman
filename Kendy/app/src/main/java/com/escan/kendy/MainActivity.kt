d302ccc318fe6f6ea71aa36079e40b24143688b8
Escan-DNMZ <84273839+Escan-DNMZ@users.noreply.github.com>
2022-02-28 20:23:06 +0300

    Update

--- Kendy/app/src/main/java/com/escan/kendy/MainActivity.kt
+++ Kendy/app/src/main/java/com/escan/kendy/MainActivity.kt
@@ -4,33 +4,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import kotlinx.android.synthetic.main.activity_main.*
+import com.escan.kendy.databinding.ActivityMainBinding
 import java.util.*
 import kotlin.collections.ArrayList
+
 
 class MainActivity : AppCompatActivity() {
 
     var score = 0
     val imageArray = ArrayList<ImageView>()
-    lateinit var handler:Handler
+     var handler:Handler = Handler(Looper.myLooper()!!)
     lateinit var runnable: Runnable
     lateinit var countDownTimer: CountDownTimer
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root //Layout ile çalışacağımız belirtiyor
+        setContentView(view)
 
+        //gridLayout içinde olan itemlerin sayısını alıyor.
+        val count : Int = (binding.gridLayout.getChildCount() -1)
+        // 0'dan başla count a kadar
+        for(index in 0..count){
+            imageArray.add(binding.gridLayout.getChildAt(index) as ImageView)
+        }
+        hideImages()
 
         countDownTimer = object : CountDownTimer(15000,1000) {
             override fun onFinish() {
-                textView.text = "0"
-                textView2.text = "Score: 0"
-
+                handler.removeCallbacks(runnable)
+                for (image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
+                binding.textView.text = "0"
+                binding.textView2.text = "Score: 0"
                 val alert = AlertDialog.Builder(this@MainActivity)
                 alert.setTitle("Game Over")
                 alert.setMessage("Are you want restart ?")
@@ -39,6 +55,7 @@ class MainActivity : AppCompatActivity() {
                     val intent = intent
                     finish()
                     startActivity(intent)
+                    handler.removeCallbacks(runnable)
 
                 }
                 alert.setNegativeButton("No"){ dialog,which ->
@@ -49,14 +66,31 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTick(p0: Long) {
-                textView.text = "Score: ${p0/1000}"
+                binding.textView.text = "${p0/1000}"
+
             }
         }.start()
     }
 
     fun increaseScore(view: View){
         score = score + 1
-        textView2.text = "$score"
+        binding.textView2.text = "Score: $score"
 
+    }
+
+    fun hideImages(){
+        runnable = object : Runnable {
+            override fun run() {
+                for (image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
+                val random = Random()
+                val randomIndex = random.nextInt(15)
+                imageArray[randomIndex].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable,600)
+            }
+        }
+            handler.post(runnable)
     }
 }
